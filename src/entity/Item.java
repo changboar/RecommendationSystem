0dbd3660215e0128a7a8a4b857ccec90015e5ad9
chhdivinity <r99522712@ntu.edu.tw>
2018-11-17 21:05:37 -0800

    stage 1 done

---
+++ src/entity/Item.java
@@ -0,0 +1,142 @@
+package entity;
+
+import java.util.Set;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Item {
+	
+	private String itemId;
+	private String name;
+	private double rating;
+	private String address;
+	private Set<String> categories;
+	private String imageUrl;
+	private String url;
+	private double distance;
+	
+	
+	/**
+	 * This is a builder pattern in Java.
+	 */
+	private Item(ItemBuilder builder) {
+		this.itemId = builder.itemId;
+		this.name = builder.name;
+		this.rating = builder.rating;
+		this.address = builder.address;
+		this.categories = builder.categories;
+		this.imageUrl = builder.imageUrl;
+		this.url = builder.url;
+		this.distance = builder.distance;
+	}
+
+	
+	public String getItemId() {
+		return itemId;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public Set<String> getCategories() {
+		return categories;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	
+	public JSONObject toJSONObject() {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("item_id", itemId);
+			obj.put("name", name);
+			obj.put("rating", rating);
+			obj.put("address", address);
+			obj.put("categories", new JSONArray(categories));
+			obj.put("image_url", imageUrl);
+			obj.put("url", url);
+			obj.put("distance", distance);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return obj;
+	}
+	
+	// use builder pattern
+	// 1. can unordered sort, 
+	// 2. handle null parameter
+	// most important: 
+	// 3. no need to build several constructor
+	
+	// ***不可以沒static，若沒static，你必須要先有instance!!!
+	// 因後續創建要先建Itembuilder，再建出Item
+	// ***ItemBuilder必須是inner class，不能平行item
+	// 因為這樣才能調用到Item裡的private constructor
+	// ***不能public，才不會暴露
+	public static class ItemBuilder {
+		private String itemId;
+		private String name;
+		private double rating;
+		private String address;
+		private Set<String> categories;
+		private String imageUrl;
+		private String url;
+		private double distance;
+				
+		public void setItemId(String itemId) {
+			this.itemId = itemId;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public void setRating(double rating) {
+			this.rating = rating;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public void setCategories(Set<String> categories) {
+			this.categories = categories;
+		}
+
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+
+		public void setUrl(String url) {
+			this.url = url;
+		}
+
+		public void setDistance(double distance) {
+			this.distance = distance;
+		}
+		
+		public Item build() {
+			return new Item(this);
+		}
+	}
+}
+//Item i = new Item();
+//JSONObject obj = i.toJSONObject();
+
+
+//ItemBuilder itemBuilder = new ItemBuilder();
+//itemBuilder.setName().setItemId();
+//Item item = itemBuilder.build();
+
